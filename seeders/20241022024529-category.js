252ce592d68a1fbaa045cf98b28e061c2bde5bae
zfl19 <19aleiya@gmail.com>
2024-10-22 12:11:15 +0800

    添加 'await' 关键字

--- seeders/20241022024529-category.js
+++ seeders/20241022024529-category.js
@@ -3,15 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await queryInterface.bulkInsert('Categories', [
       { name: '前端开发', rank: 1, createdAt: new Date(), updatedAt: new Date() },
       { name: '后端开发', rank: 2, createdAt: new Date(), updatedAt: new Date() },
@@ -23,12 +14,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.bulkDelete('Categories', null, {})
   }
 };
